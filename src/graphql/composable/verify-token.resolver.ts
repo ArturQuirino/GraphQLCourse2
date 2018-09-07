51402c24a36fce1807cb8b209a687bda12a82593
Artur Quirino <arturquirino6@gmail.com>
2018-09-07 10:22:09 -0300

    Bug fix and add authentication to users

--- src/graphql/composable/verify-token.resolver.ts
+++ src/graphql/composable/verify-token.resolver.ts
@@ -9,11 +9,10 @@ export const verifyTokenResolver: ComposableResolver<any, ResolverContext> =
   (resolver: GraphQLFieldResolver<any, ResolverContext>): GraphQLFieldResolver<any, ResolverContext> => {
     return (parent, args, context: ResolverContext, info) => {
       const token: string = context.authorization ? context.authorization.split(' ')[1] : undefined;
-      jwt.verify(token, JWT_SECRET, (err, decoded: any) => {
+      return jwt.verify(token, JWT_SECRET, (err, decoded: any) => {
         if (!err) {
           return resolver(parent, args, context, info);
         }
-
         throw new Error(`${err.name}: ${err.message}`);
       });
     };
